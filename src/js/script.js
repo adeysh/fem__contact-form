3b14227232546b709c1d1e12f6bba81f665f2a99
Adesh Katiya <adeshkatiya27@gmail.com>
2025-05-21 19:58:50 +0530

    feat: add error handling logic with basic validation of form fields
    added validation for form fields and show error messages on invalid input, styled error message elements, added styles to input text

---
+++ src/js/script.js
@@ -0,0 +1,104 @@
+const form = document.getElementById("contact-form");
+
+
+function isAlphabetic(str) {
+    return /^[a-zA-Z]+$/.test(str);
+}
+
+function validateEmail(email) {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(form));
+    console.log(data);
+    let isValid = true;
+
+    for (const field in data) {
+        const fieldValue = data[field];
+        let input = null;
+
+        if (field == 'query-type') {
+            input = document.getElementById(fieldValue);
+        } else {
+            input = document.getElementById(field);
+        }
+        const errorEl = document.getElementById(`${field}-error`);
+
+        if (!('query-type' in data)) {
+            isValid = false;
+            const errorEl = document.getElementById("query-type-error");
+            errorEl.classList.remove("sr-only");
+            errorEl.classList.add("error-visible");
+            input.classList.add("error-visible");
+            input.setAttribute("aria-invalid", "true");
+        }
+
+        if (!('consent' in data)) {
+            isValid = false;
+            const errorEl = document.getElementById("consent-error");
+            errorEl.classList.remove("sr-only");
+            errorEl.classList.add("error-visible");
+            input.classList.add("error-visible");
+            input.setAttribute("aria-invalid", "true");
+        }
+
+
+
+        if (fieldValue === '') {
+            isValid = false;
+            errorEl.classList.remove("sr-only");
+            errorEl.classList.add("error-visible");
+            input.classList.add("error-visible");
+            input.setAttribute("aria-invalid", "true");
+        } else {
+            errorEl.classList.remove("error-visible");
+            errorEl.classList.add("sr-only");
+            input.classList.remove("error-visible");
+            input.removeAttribute("aria-invalid");
+
+            if (field === "first-name" || field === "last-name") {
+                const isInputValid = isAlphabetic(fieldValue);
+                const errorMessageEl = document.getElementById(`${field}-pattern`);
+                if (!isInputValid) {
+                    console.log(errorMessageEl);
+                    errorMessageEl.classList.remove("sr-only");
+                    errorMessageEl.classList.add("error-visible");
+                    console.log(input);
+                    input.classList.add("error-visible");
+                    input.setAttribute("aria-invalid", "true");
+                    isValid = false;
+                } else {
+                    errorMessageEl.classList.add("sr-only");
+                    errorMessageEl.classList.remove("error-visible");
+                    console.log(input);
+                    input.classList.remove("error-visible");
+                    input.removeAttribute("aria-invalid");
+                }
+            }
+
+            if (field === "email") {
+                const isEmailValid = validateEmail(fieldValue);
+                const errorMessageEl = document.getElementById("email-pattern");
+
+                if (!isEmailValid) {
+                    errorMessageEl.classList.remove("sr-only");
+                    errorMessageEl.classList.add("error-visible");
+
+                    input.classList.add("error-visible");
+                    input.setAttribute("aria-invalid", "true");
+                    isValid = false;
+                } else {
+                    errorMessageEl.classList.add("sr-only");
+                    errorMessageEl.classList.remove("error-visible");
+                    console.log(input);
+                    input.classList.remove("error-visible");
+                    input.removeAttribute("aria-invalid");
+                }
+            }
+        }
+    }
+});
